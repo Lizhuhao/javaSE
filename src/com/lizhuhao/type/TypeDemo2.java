e44d697a1b34f6408beff3afdbe94088fe27978e
Lizhuhao <1737849099@qq.com>
2023-04-21 20:01:39 +0800

    表达式的自动类型转换规则

---
+++ src/com/lizhuhao/type/TypeDemo2.java
@@ -0,0 +1,17 @@
+package com.lizhuhao.type;
+
+public class TypeDemo2 {
+    public static void main(String[] args) {
+        byte a = 10;
+        int b = 20;
+        double c = 2.0;
+        //int d = a + b + c;
+        double rs = a + b + c;
+        System.out.println(rs);
+
+        byte i = 10;
+        byte j = 20;
+        //byte k = i + j;
+        int k = i + j;
+    }
+}
