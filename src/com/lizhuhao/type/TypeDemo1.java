434b2f480638669dfb00dc686f5703f520e80b91
Lizhuhao <1737849099@qq.com>
2023-04-21 19:44:01 +0800

    自动类型转换

---
+++ src/com/lizhuhao/type/TypeDemo1.java
@@ -0,0 +1,11 @@
+package com.lizhuhao.type;
+
+public class TypeDemo1 {
+    //理解自动类型转换
+    public static void main(String[] args) {
+        byte a=20;
+        int b=a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
