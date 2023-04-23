907a4dd85e38b5df3a26c15d32440d08961dff9b
Lizhuhao <1737849099@qq.com>
2023-04-23 20:55:41 +0800

    冒泡排序

---
+++ src/com/lizhuhao/array/Test4.java
@@ -0,0 +1,21 @@
+package com.lizhuhao.array;
+
+public class Test4 {
+    //冒泡排序
+    public static void main(String[] args) {
+        int[] arr = {5,2,3,1};
+        int temp;
+        for (int i = 0; i < arr.length-1; i++) {
+            for(int j = 0; j < arr.length - i - 1; j++){
+                if(arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
